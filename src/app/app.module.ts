b5140f882e39284de8928aa3618f910025baade7
Prashant Singh Vinay Kumar Singh <prashantsingh.vinaykumarsingh@msc.com>
2023-09-04 11:54:42 +0530

    Basic setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HousingLocationComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-housing-location',
+  templateUrl: './housing-location.component.html',
+  styleUrls: ['./housing-location.component.css']
+})
+
+export class HousingLocationComponent {
+}
